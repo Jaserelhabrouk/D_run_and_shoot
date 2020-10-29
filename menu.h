ec504e2d6070485c88d8446f1d0aa1c80e836f28
Pari-Firoozi <72401886+Pari-Firoozi@users.noreply.github.com>
2020-10-29 19:15:39 -0400

    Update menu.h

--- menu.h
+++ menu.h
@@ -17,7 +17,7 @@
  * @enum menu_items_t
  * The enumeration of menu items.
  */
-typedef enum menu_items_t {
+typedef enum menu_items {
 	MENU_ITEM_USER_MANUAL,
 	MENU_ITEM_SINGLE_PLAYER,
 	MENU_ITEM_MULTI_PLAYER,
@@ -31,7 +31,7 @@ typedef enum menu_items_t {
  * @typedef item_t
  * A structure represents items' name.
  */
-typedef struct item_t {
+typedef struct item {
 	char str[MAX_ITEM_SIZE];    /**< the name of the item in the menu*/
 } item_t;
 
@@ -39,7 +39,7 @@ typedef struct item_t {
  * @typedef menu_t
  * A structure represents menu item.
  */
-typedef struct menu_t {
+typedef struct menu {
 	menu_items_t selector;
 	item_t items[MENU_ITEM_NUM_OF_ITEMS ];
 } menu_t;

9102bb01e3bdd3a8c6e2f2cc0c4a260a775c9bfd
Pari Firoozi <parichehrehfiroozish@cmail.carleton.ca>
2020-10-28 20:23:49 -0400

    Added choose_menu_item function and its test in choose_menu_item.c file. The required type definitions are in menu.h file. Added load_map function and its test function in load_map.c file. The required type definitions are in map.h file.

---
+++ choose_menu_item.c
@@ -0,0 +1,88 @@
+/**
+ * @file choose_menu_item.c
+ * @author Pari
+ * @brief Chooses menu items and test it.
+ *
+ * This file chooses a menu item and test if the function is working.
+ */
+#include <stdio.h>
+#include <assert.h>
+#include "menu.h"
+
+void credit(){
+	printf("credit\n");
+}
+
+void exit_game(){
+	printf("exit\n");
+}
+
+void multi_player() {
+	printf("multi_player\n");
+}
+
+void single_player() {
+	printf("single_player\n");
+}
+
+void user_manual() {
+	printf("user_manual\n");
+}
+
+void option(){
+	printf("option\n");
+}
+
+
+/**
+ * @brief chooses a menu item.
+ *
+ * This function choose one of the menu items based on the selector.
+ *
+ * @param [in] menu which is a structure.
+ * @return void.
+ */
+void choose_menu_item(menu_t menu) {
+	switch (menu.selector)
+	{
+		case MENU_ITEM_USER_MANUAL:
+			user_manual();
+			break;
+		case MENU_ITEM_SINGLE_PLAYER:
+			single_player();
+			break;
+		case MENU_ITEM_MULTI_PLAYER:
+			multi_player();
+			break;
+		case MENU_ITEM_OPTION:
+			option();
+			break;
+		case MENU_ITEM_CREDIT:
+			credit();
+			break;
+		case MENU_ITEM_EXIT:
+			exit_game();
+			break;
+		default:
+			assert(FALSE);
+			break;
+	}
+}
+
+/**
+ * @brief tests choose_menu_item() functions.
+ *
+ * This is a test function for choose_menu_item() and check if it performs correctly.
+ * It has no input.
+ *maa
+ * @return 0 in success
+ */
+int test_choose_menu_item() {
+	menu_t menu;
+	for (int item = 0; item < MENU_ITEM_NUM_OF_ITEMS; item++)
+	{
+		menu.selector = item;
+		choose_menu_item(menu);
+	}
+	return 0;
+}
---
+++ map.h
@@ -0,0 +1,108 @@
+/**
+ * @file map.h
+ * @author Pari
+ * @brief decleration of the map
+ *
+ * Function decleration and type definition for the game's map.
+ */
+#ifndef MAP_H_
+#define MAP_H_
+
+#define MAP_MAX_NUM_OF_BARRIERS 10
+#define MAP_MAX_NUM_OF_ARROWS 20
+
+/**
+ * @typedef position_t
+ * A structure represents the position.
+ */
+typedef struct position_t {
+	int x;             /**< position in x coordinate */
+	int y;             /**< position in y coordinate */
+} position_t;
+
+/**
+ * @enum direction_t
+ * The enumeration of movement direction.
+ */
+typedef enum direction_t {
+	DIRECTION_RIGHT,
+	DIRECTION_LEFT,
+	DIRECTION_UP,
+	DIRECTION_DOWN
+} direction_t;
+
+/**
+ * @enum speed_t
+ * The enumeration of speed level.
+ */
+typedef enum speed_t {
+	SPEED_LOW,
+	SPEED_NORMAL,
+	SPEED_HIGH,
+} speed_t;
+
+/**
+ * @typedef map_space_t
+ * A structure represents the space which map is defined in it.
+ */
+typedef struct map_space_t {
+	int x_min;
+	int x_max;
+	int y_min;
+	int y_max;
+} map_space_t;
+
+/**
+ * @typedef map_barrier_t
+ * A structure represents the a barrier in the map.
+ */
+typedef struct map_barrier_t {
+	position_t current_pos;     /**< current position of a barrier*/
+	int length;                 /**< length of a barrier*/
+	direction_t current_dir;    /**< current direction of a barrier*/
+} map_barrier_t;
+
+/**
+ * @typedef arrow_t
+ * A structure represents an arrow in the map
+ */
+typedef struct arrow_t {
+	position_t current_pos;    /**< current position of an arrow*/
+	speed_t speed;             /**< the speed of an arrow*/
+	direction_t direction;     /**< direction of an arrow*/
+} arrow_t;
+
+/**
+ * @typedef player_t
+ * A structure represents a player in the map
+ */
+typedef struct player_t {
+	char name;                 /**< name of the player*/
+	position_t current_pos;    /**< current position of the player*/
+	int heart;                 /**< number of the heart the player have*/
+} player_t;
+
+/**
+ * @typedef goal_t
+ * A structure represents the goal in the map
+ */
+typedef struct goal_t {
+	position_t pos;         /**< position of the goal in the map*/
+} goal_t;
+
+/**
+ * @typedef map_t
+ * A structure represents the map
+ */
+typedef struct map_t {
+	map_space_t space;                                 /**< space of the map*/
+	int number_of_barriers;                            /**< number of barriers in the map*/
+	int number_of_arrows;                              /**< number of arrows in the map*/
+	map_barrier_t barrier[MAP_MAX_NUM_OF_BARRIERS];    /**< barries in the map*/
+	arrow_t arrow[MAP_MAX_NUM_OF_ARROWS];              /**< arrows in the map*/
+	goal_t goal;                                       /**< goal in the map*/
+	player_t player;                                   /**< player in the map*/
+} map_t;
+
+#endif /* MAP_H_ */
+

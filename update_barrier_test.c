5d74658f2235fef72a6c0491d2bbe90bb1de37e0
JeanneXu1122 <72245881+JeanneXu1122@users.noreply.github.com>
2020-11-02 10:02:51 +0800

    adjust code files
    adjust code files according to comments

--- update_barrier_test.c
+++ update_barrier_test.c
@@ -26,22 +26,3 @@ void update_barrier_test()
     assert(barriers[7].current_pos.y == 98);
 
 }
-
-void take_heart_test() {
-
-    player_t p1 = { "Mark",{.x = 20,.y = 20},2 };
-    player_t p2 = { "Mark",{.x = 40,.y = 20},1 };
-    player_t p3 = { "Marry",{.x = 30,.y = 10},0 };
-    player_t p4 = { "Marry",{.x = 30,.y = 10},-1 };
-   
-    take_heart(&p1);
-    take_heart(&p2);
-    take_heart(&p3);
-    take_heart(&p4);
-    take_heart(NULL);
-
-    assert(p1.heart == 1);
-    assert(p2.heart == 0);
-    assert(p3.heart == 0);
-    assert(p4.heart == -1);
-}

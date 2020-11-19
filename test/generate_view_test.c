e157823e15c8bf692a42ae630085f96afb51c9dc
JeanneXu1122 <72245881+JeanneXu1122@users.noreply.github.com>
2020-11-19 20:29:15 +0800

    update code according to Pari's comment

--- test/generate_view_test.c
+++ test/generate_view_test.c
@@ -9,23 +9,24 @@
 #include <sdl_ttf.h>
 #include <SDL_image.h>
 #include <stdbool.h>
-#include "map.h"
+#include"map.h"
+
+extern SDL_Window* g_window;
+extern SDL_Renderer* g_renderer;
+
+extern TTF_Font* g_font;
+/**define the constants representing different texture*/
+extern SDL_Texture* g_player_texture;
+extern SDL_Texture* g_barrier_texture;
+extern SDL_Texture* g_arrow_down_texture;
+extern SDL_Texture* g_arrow_up_texture;
+extern SDL_Texture* g_goal_texture; 
 
 
 /** define the width and height of the scrren.*/
-const int SCREEN_WIDTH = 1024;
-const int SCREEN_HEIGHT = 768;
+extern int g_screen_width;
+extern int g_screen_height;
 
-SDL_Window* g_window = NULL;
-SDL_Renderer* g_renderer = NULL;
-
-TTF_Font* g_font = NULL;
-/**define the global variables representing different texture*/
-SDL_Texture* g_player_texture = NULL;
-SDL_Texture* g_barrier_texture = NULL;
-SDL_Texture* g_arrow_down_texture = NULL;
-SDL_Texture* g_arrow_up_texture = NULL;
-SDL_Texture* g_goal_texture = NULL;
 
 SDL_Texture* Load_image(const char* filePath)
 {
@@ -45,40 +46,37 @@ SDL_Texture* Load_image(const char* filePath)
 
 int generate_view_test()
 {
-    //update_barrier_test();
-    //take_heart_test();
-
-    /**Initialize the SDL*/
+     /**Initialize the SDL*/
     if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
     {
         exit(-1);
     }
+    atexit(SDL_Quit);// 注册SDL_Quit，当退出时调用，使得退出时程序自动清理
 
-    /**Initialize Image library*/
-    IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG);
+
+
+    /**Load the map*/
+    char file_path[1000] = "data/map.txt";
+    map_t map = load_map(file_path);
+
+    g_screen_width = map.space.x_max - map.space.x_min;
+    g_screen_height = map.space.y_max - map.space.y_min;
 
     /**Create the window*/
-    g_window = SDL_CreateWindow("GAME WINDOW", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    g_window = SDL_CreateWindow("GAME WINDOW", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, g_screen_width, g_screen_height, SDL_WINDOW_SHOWN);
     if (NULL == g_window)
     {
         SDL_Quit();
         return -1;
     }
 
-    /**Initialize font library*/
-    if (TTF_Init() == -1)
-        return -1;
-    /**Open the font*/
- /*   g_font = TTF_OpenFont("simkai.ttf", 16);
-    if (!g_font)
-    {
-        printf("TTF_OpenFont: Open simsun.ttf %s\n", TTF_GetError());
-        return -1;
-    }*/
-
-
     /**Create the Renderer*/
     g_renderer = SDL_CreateRenderer(g_window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    /**Initialize Image library*/
+    IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG);
+
+  
 
     /**Load the image resource*/
     g_player_texture = Load_image("data/player.jpg");
@@ -86,12 +84,9 @@ int generate_view_test()
     g_arrow_down_texture = Load_image("data/arrow_down.jpg");
     g_arrow_up_texture = Load_image("data/arrow_up.jpg");
     g_goal_texture = Load_image("data/goal.jpg");
-
-
-    /**Load the map*/
-    char file_path[1000] = "data/map.txt";
-    map_t map = load_map(file_path);
-
+    /**Initialize font library*/
+    if (TTF_Init() == -1)
+        return -1;
     /**Load Texture of player, target,barriers and arrows*/
 
     /**Check if it's the phase of operating the menu*/
@@ -113,59 +108,10 @@ int generate_view_test()
             switch (event.type)
             {
             case SDL_QUIT:
-                game_running = true;
+                game_running = false;
                 break;
             case SDL_KEYDOWN:/**< User presses a key*/
-            {
-                if (is_menu)
-                {
-                    /**Menu select*/
-                    switch (event.key.keysym.sym)
-                    {
-                    case SDLK_UP:
-
-                        break;
-
-                    case SDLK_DOWN:
-                        break;
-
-                    case SDLK_LEFT:
-                        break;
-
-                    case SDLK_RIGHT:
-                        break;
-
-                    default:
-                        break;
-
-                    }
-                }
-                else
-                {
-                    /**Game playing*/
-                    switch (event.key.keysym.sym)
-                    {
-                    case SDLK_UP:
-                        /**Check if collision between player and barriers*/
-
-                        /**Update position of the player*/
-
-
-                        break;
-
-                    case SDLK_DOWN:
-                        break;
-
-                    case SDLK_LEFT:
-                        break;
-
-                    case SDLK_RIGHT:
-                        break;
-
-                    default:
-                        break;
-                    }
-                }
+            {                
                 break;
             }
             default:
@@ -174,7 +120,6 @@ int generate_view_test()
 
         }
 
-
         /**Clear the cashe region*/
         SDL_RenderClear(g_renderer);
         SDL_SetRenderDrawColor(g_renderer, 0xff, 0xff, 0xff, 0xff);
@@ -182,10 +127,7 @@ int generate_view_test()
         /**Check if collision between player and arrows*/
 
         /**Draw all objects*/
-        generate_view(&map, g_renderer, g_player_texture, g_goal_texture, g_barrier_texture, g_arrow_down_texture, g_arrow_up_texture);
-
-        /**Display the content to render*/
-        SDL_RenderPresent(g_renderer);
+        generate_view(&map);
     }
 
     TTF_CloseFont(g_font);
